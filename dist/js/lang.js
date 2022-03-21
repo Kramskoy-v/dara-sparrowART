8ac3497d2dd2851cc1455588010bee938a198479
Kramskoy-v <71666173+Kramskoy-v@users.noreply.github.com>
2022-03-21 12:34:20 +0300

    first commit

---
+++ dist/js/lang.js
@@ -0,0 +1,54 @@
+const langArr = {
+	'title' : {
+		'ru' : 'Dara Sparrow - цифровой художник',
+		'en' : 'Dara Sparrow - a digital artist',
+	},
+
+	'about': {
+		'ru': 'Цифровой художник/иллюстратор.' + '<br>' + 'На этом сайте вы найдёте мои работы и 		контакты для сотрудничества.',
+		
+		'en' : 'A digital artist/illustrator.' + '<br>' + ' Here you can find my works and contacts for collaboration.',
+    },
+
+	'link-home' : {
+		'ru' : 'главная',
+		'en' : 'home',
+	},
+
+	'link-portfolio' : {
+		'ru' : 'портфолио',
+		'en' : 'portfolio',
+	},
+
+	'link-contacts' : {
+		'ru' : 'контакты',
+		'en' : 'contacts',
+	},
+
+	'connection' : {
+		'ru' : 'Свяжитесь со мной:',
+		'en' : 'Contact me:',
+	},
+
+	'form' : {
+		'ru' : 'или заполните форму ниже:',
+		'en' : 'or use the form bellow:',
+	},
+
+	'personal-data' : {
+		'ru': 'Я даю своё согласие на обработку персональных данных в соответствии с',
+		'en' : 'I give my consent to the processing of personal data in accordance with ',
+	},
+
+	'terms' : {
+		'ru' : 'Условиями',
+		'en' : 'Terms',
+	},
+
+	'button' : {
+		'ru' : 'отправить',
+		'en' : 'send',
+	}
+
+
+}
---
+++ dist/pages/send.php
@@ -0,0 +1,56 @@
+<?php
+// Файлы phpmailer
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Переменные, которые отправляет пользователь
+$name = $_POST['username'];
+$email = $_POST['useremail'];
+$textarea = $_POST['usertext'];
+
+// Формирование самого письма
+$title = "Обращение с сайта Dara Sparrow art";
+$body = "
+<h2>Новое Обращение с сайта Dara Sparrow art</h2>
+<b>Имя:</b> $name<br>
+<b>Электронная почта:</b> $email<br><br>
+<b>Текст обращения:</b> $textarea<br>";
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    //$mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.mail.ru'; // SMTP сервера вашей почты
+    $mail->Username   = 'kramskoy_va@mail.ru'; // Логин на почте
+    $mail->Password   = '****'; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('kramskoy_va@mail.ru', 'Сайт Dara Sparrow'); // Адрес самой почты и имя отправителя
+
+    // Получатель письма
+    $mail->addAddress('example@gmail.com');  
+    //$mail->addAddress('');
+
+// Отправка сообщения
+$mail->isHTML(true);
+$mail->Subject = $title;
+$mail->Body = $body;    
+
+// Проверяем отравленность сообщения
+if ($mail->send()) {$result = "success";} 
+else {$result = "error";}
+
+} catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+// Отображение результата
+echo json_encode(["result" => $result, "resultfile" => $rfile, "status" => $status]);
